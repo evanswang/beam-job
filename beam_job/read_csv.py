21d4aa7e659a365ba63f29542e63f9ac450bec09
Shicai Wang <sw23@sanger.ac.uk>
2022-06-14 10:44:42 +0100

    rm unused func; trim requirements

--- beam_job/read_csv.py
+++ beam_job/read_csv.py
@@ -11,10 +11,6 @@ from typing import Dict, Iterable, List
 def ReadCsvFiles(
     pbegin: beam.pvalue.PBegin, file_patterns: List[str]
 ) -> beam.PCollection[Dict[str, str]]:
-    def expand_pattern(pattern: str) -> Iterable[str]:
-        for match_result in beam_fs.match([pattern])[0].metadata_list:
-            yield match_result.path
-
     def read_csv_lines(file_name: str) -> Iterable[Dict[str, str]]:
         with beam_fs.open(file_name) as f:
             # Beam reads files as bytes, but csv expects strings,
@@ -25,6 +21,5 @@ def ReadCsvFiles(
     return (
         pbegin
         | "Create file patterns" >> beam.Create(file_patterns)
-        | "Expand file patterns" >> beam.FlatMap(expand_pattern)
         | "Read CSV lines" >> beam.FlatMap(read_csv_lines)
     )
